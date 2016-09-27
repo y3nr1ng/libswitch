5745f10eba8368fbd600dc5cd86d9c5fa58bfa1a
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-09-27 17:45:55 +0800

    Complete gateway connections

--- libswitch/comm/SSH.py
+++ libswitch/comm/SSH.py
@@ -7,8 +7,11 @@ import paramiko
 import logging
 
 class SSHCredential:
-    def __init__(self, hostname, username=None, password=None):
-
+    def __init__(self, host, port=22, username=None, password=None):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
 
 class SSH(object):
     def __init__(self):
@@ -16,10 +19,53 @@ class SSH(object):
         self.logger = logging.getLogger('libswitch.comm.SSH')
 
     def __enter__(self):
-        self.__load_ssh_config()
         return self
 
-    def connect(self, hostname, username=None, password=None):
+    def connect(self, cred, via=[]):
+        self.transport_list = []
 
+        # Last item in the via is the destination.
+        via.append(cred)
+
+        # Hop through the sites.
+        for v in via:
+            self.logger.debug('Connect to {}:{}'.format(v.host, v.port))
+
+            addr = (v.host, v.port)
+            if not self.transport_list:
+                t = paramiko.Transport(addr)
+            else:
+                # Get the last transport.
+                t0 = self.transport_list[-1]
+                # Start the forwarding.
+                ch = t0.open_channel('direct-tcpip', addr, ('127.0.0.1', 0))
+                t = paramiko.Transport(ch)
+            t.start_client()
+            t.auth_password(v.username, v.password)
+            self.transport_list.append(t)
+
+        # Open the session.
+        t = self.transport_list[-1]
+        self.session = t.open_session()
+
+        #DEBUG
+        self.send('sh ?')
+        res = self.receive()
+        self.logger.info(res)
+
+    def send(self, cmd):
+        self.session.exec_command(cmd)
+        ret = self.session.recv_exit_status()
+        return ret
+
+    def receive(self, size=1024):
+        buf = ''
+        while self.session.recv_ready():
+            buf += self.__decode_byte_stream(self.session.recv(size))
+        return buf
+
+    def __decode_byte_stream(self, b):
+        return b.decode('utf-8', errors='ignore')
 
     def __exit__(self, e_type, e_val, trace) :
+        print('shit')
--- tests/main_test.py
+++ tests/main_test.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 import libswitch.comm.SSH as Comm
-from secret import hops, sw_host, sw_user, sw_pass
+from secret import vias, target
 
-c = Comm(hops=hops)
-c.connect(sw_host, username=sw_user, password=sw_pass)
+c = Comm()
+c.connect(target, via=vias)
