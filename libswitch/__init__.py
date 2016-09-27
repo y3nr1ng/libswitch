0add28a5600d07c740578f2190d3ca2b68cec8cb
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-09-27 12:15:16 +0800

    Use logger in the SSH module

--- libswitch/__init__.py
+++ libswitch/__init__.py
@@ -13,7 +13,7 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 
 # Create the formatter and add it to the handlers.
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(name)s [%(levelname)s] %(message)s')
 #fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 
--- libswitch/comm/SSH.py
+++ libswitch/comm/SSH.py
@@ -4,9 +4,13 @@ This module facilitate connection with the switch through SSH.
 
 import os
 import paramiko
+import logging
 
 class SSH(object):
     def __init__(self, hops=None) :
+        # Set the logger.
+        self.logger = logging.getLogger('libswitch.comm.SSH')
+
         # Intermediate SSH hops.
         self.hops = hops
 
@@ -16,6 +20,10 @@ class SSH(object):
         # Setup the policies.
         self.handler.load_system_host_keys()
         self.handler.set_missing_host_key_policy(paramiko.WarningPolicy())
+
+    def __enter__(self) :
+        self.__load_ssh_config()
+        return self
 
     def __load_ssh_config(self) :
         """
@@ -33,16 +41,7 @@ class SSH(object):
         print('Dump the SSH config')
         print(self.ssh_config)
 
-    def __enter__(self) :
-        self.__load_ssh_config()
-        return self
-
-    def __establish(self) :
-        """
-        Establish the SSH connection.
-        """
-
-    def __connect(self, host, username=None, password=None) :
+    def __connect(self, hostname, username=None, password=None) :
         """
         The actual function that establish the connection.
         """
@@ -50,16 +49,54 @@ class SSH(object):
         # Lookup in the config file.
         explicit_config = self.ssh_config.lookup(host)
 
-        if 'hostname'
+        # Override the hostname.
+        if 'hostname' in explicit_config :
+            config['hostname'] = explicit_config['hostname']
+        else :
+            config['hostname'] = hostname
+
+        # Override the username if not assigned.
         if username is None :
+            if 'username' in explicit_config :
+                config['username'] = explicit_config['username']
+            else :
+                self.logger.error('Username not provided')
+        else :
+            config['username'] = username
 
+        # Override the password if not assigned.
         if password is None :
+            # Use the identity file first.
+            if 'identityfile' in explicit_config :
+                config['key_filename'] = explicit_config['identityfile']
+            elif 'password' in explicit_config :
+                config['password'] = explicit_config['password']
+            else :
+                self.logger.error('Nor keyfile or password is provided')
+        else :
+            config['password'] = password
 
+        # Override the port.
+        if 'port' in explicit_config :
+            config['port']  = explicit_config['port']
 
-    def connect(self, host, username=None, password=None) :
+        # Override the proxy.
+        if 'proxycommand' in explicit_config :
+            config['sock'] = paramiko.ProxyCommand(explicit_config['proxycommand'])
+
+        # Establish the connection.
+        try :
+            self.handler.connect(config)
+        except paramiko.AuthenticationException :
+            self.logger.error('Authentication failed')
+        except Exception as e :
+            self.logger.error(str(e))
+
+    def connect(self, hostname, username=None, password=None) :
         # Establish the connection with hops first.
         if hops :
-            for h in hops :
+            print('not support')
+
 
 
 
