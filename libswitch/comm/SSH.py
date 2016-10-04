c1e4c8ea5c0f44638159f8e90ca47fb0b4f7327b
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-10-04 14:22:46 +0800

    Dirty patch the continuous exec_cmd

--- libswitch/comm/SSH.py
+++ libswitch/comm/SSH.py
@@ -2,32 +2,64 @@
 This module facilitate connection with the switch through SSH.
 """
 
+import logging
 import os
 import paramiko
-import logging
 
 class SSHCredential:
+    """
+    SSHCredential defines the connection details of a SSH channel.
+    """
+    config_path = ''
+    config = None
+
     def __init__(self, host, port=22, username=None, password=None):
+        # Load local SSH config files if not loaded.
+        if not self.config_path:
+            self._find_ssh_config()
+            self._load_ssh_config()
+
         self.host = host
         self.port = port
         self.username = username
         self.password = password
+
+    @staticmethod
+    def _find_ssh_config():
+        p = os.path.expanduser('~/.ssh/config')
+        if not os.path.exists(p):
+            p = 'invalid'
+        SSHCredential.config_path = p
+
+    @staticmethod
+    def _load_ssh_config():
+        if SSHCredential.config_path == 'invalid':
+            return
+
+        # Instantiate the SSHConfig object.
+        SSHCredential.config = paramiko.SSHConfig()
+        # Load the file.
+        with open(SSHCredential.config_path) as f:
+            SSHCredential.config.parse(f)
 
 class SSH(object):
     def __init__(self):
         # Set the logger.
         self.logger = logging.getLogger('libswitch.comm.SSH')
 
-    def connect(self, cred, via=[]):
+        # Define the variables.
+        self.transport_list = []
+        self.channel = None
+
+    def connect(self, dest_cred, via=None):
         self.transport_list = []
 
         # Last item in the via is the destination.
-        via.append(cred)
+        via.append(dest_cred)
 
         # Hop through the sites.
         for v in via:
             addr = (v.host, v.port)
-            self.logger.debug('Connect to {}:{}'.format(*addr))
             if not self.transport_list:
                 t = paramiko.Transport(addr)
             else:
@@ -38,24 +70,44 @@ class SSH(object):
                 t = paramiko.Transport(ch)
             t.start_client()
             t.auth_password(v.username, v.password)
+            self.logger.debug('Connected to %s@%s:%d', v.username, v.host, v.port)
             self.transport_list.append(t)
 
-        # Open the session.
-        t = self.transport_list[-1]
-        self.session = t.open_session()
+        self.dest = dest_cred
 
     def send(self, cmd):
-        self.session.exec_command(cmd)
-        ret = self.session.recv_exit_status()
+        # Open a shell session.
+        t = self.transport_list.pop()
+        ch = t.open_session()
+
+        # Execute the command and get the return status.
+        ch.exec_command(cmd)
+        ret = ch.recv_exit_status()
+
+        self.channel = ch
+
+        #TODO: Dirty hack.
+        # Re-establish the transport.
+        addr = (self.dest.host, self.dest.port)
+        # Get the last transport.
+        t0 = self.transport_list[-1]
+        # Start the forwarding.
+        ch = t0.open_channel('direct-tcpip', addr, ('127.0.0.1', 0))
+        t = paramiko.Transport(ch)
+        t.start_client()
+        t.auth_password(self.dest.username, self.dest.password)
+        self.transport_list.append(t)
+
         return ret
 
     def receive(self, batch_size=1024):
         buf = ''
-        while self.session.recv_ready():
-            buf += self._decode_byte_stream(self.session.recv(batch_size))
+        while self.channel.recv_ready():
+            buf += self._decode_byte_stream(self.channel.recv(batch_size))
         return buf
 
-    def _decode_byte_stream(self, b):
+    @staticmethod
+    def _decode_byte_stream(b):
         return b.decode('utf-8', errors='ignore')
 
     def __exit__(self, e_type, e_val, trace) :
--- tests/main_test.py
+++ tests/main_test.py
@@ -7,11 +7,24 @@ from secret import vias, target
 
 import logging
 logger = logging.getLogger('libswitch.tests')
+import time
 
 c = Comm()
 c.connect(target, via=vias)
 
 #DEBUG
-c.send('show clu mem')
+c.send('sh ver')
+res = c.receive()
+logger.info(res)
+
+c.send('sh clu mem')
+res = c.receive()
+logger.info(res)
+
+c.send('sh proc cpu hist')
+res = c.receive()
+logger.info(res)
+
+c.send('sh proc cpu sorted 5s')
 res = c.receive()
 logger.info(res)
