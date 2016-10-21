d597bf3df45156e6d158568902cd4e1536c8c628
Liu Yen-Ting <windows.linux.mac@gmail.com>
2016-10-21 10:55:06 +0800

    Adapt to new import schema

--- tests/main_test.py
+++ tests/main_test.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-import libswitch.comm.ssh as Comm
+from libswitch.comm import SSH as Comm
 from secret import vias, target
 
 import logging
